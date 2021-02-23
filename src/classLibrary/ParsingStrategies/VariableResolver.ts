8839308b71c8772a068e04a000f884c26be7632e
Lucria <jerryzhang28eclipse@gmail.com>
2021-02-23 10:19:38 +0800

    [Chore] Reformatted interfaces and dummy variableResolver

---
+++ src/classLibrary/ParsingStrategies/VariableResolver.ts
@@ -0,0 +1,20 @@
+import { CyanSafe, FileSystemInstance, IFileSystemInstanceMetadata, IParsingStrategy } from "../interfaces/cyan";
+
+class VariableResolver implements IParsingStrategy {
+
+    ResolveContents(cyan: CyanSafe, files: FileSystemInstance[]): FileSystemInstance[] {
+        throw new Error("Method not implemented.");
+    }
+
+    Count(cyan: CyanSafe, files: FileSystemInstance[]): Map<string, number> {
+        throw new Error("Method not implemented.");
+    }
+
+    ResolveFiles(cyan: CyanSafe, files: IFileSystemInstanceMetadata[]): IFileSystemInstanceMetadata[] {
+        throw new Error("Method not implemented.");
+    }
+
+}
+
+const variableResolver = new VariableResolver();
+export {variableResolver};
--- src/classLibrary/interfaces/cyan.ts
+++ src/classLibrary/interfaces/cyan.ts
@@ -1,80 +1,92 @@
 import { of, Union } from "ts-union";
 
-interface Glob {
-    root: string
-    pattern: string[] | string
-    ignore: string | string[]
-}
-
-const FileContent = Union({
-    String: of<string>(),
-    Buffer: of<Buffer>(),
-  });
-type FileContent = typeof FileContent.T
-
 type Syntax = [string,string] // [open, close] , [ ["~", "~"] , ["{{"], ["}}"]]
 
 interface CyanObject {
-    globs: Glob[] | Glob
-    copyOnly: Glob[] | Glob // Doesn't go through parser
-    variable: object
-    flags: object
-    guid: string[] | string // ["asdf-asdf-asdf-asdf", "1234'1234'1234"]
-    syntax: Syntax[] | Syntax
-    plugins: {[s:string] : string[]} // {"api.cyanprint.dev": ["npm, c#", "github"]}
-    comments: string[] | string // remove these strings if cyanprint templates starts with them
-    pluginData: object | null |undefined
+	globs: Glob[] | Glob;
+	copyOnly: Glob[] | Glob; // Doesn't go through parser
+	variable: object;
+	flags: object;
+	guid: string[] | string; // ["asdf-asdf-asdf-asdf", "1234'1234'1234"]
+	syntax: Syntax[] | Syntax;
+	plugins: { [s: string]: string[] }; // {"api.cyanprint.dev": ["npm, c#", "github"]}
+	comments: string[] | string; // remove these strings if cyanprint templates starts with them
+	pluginData: object | null | undefined;
 }
 
 // Parsed version
 interface CyanSafe {
-    globs: Glob[]
-    copyOnly: Glob[]
-    variable: object
-    flags: object
-    guid: string[]
-    syntax: Syntax[]
-    plugins: {[s:string] : string[]}
-    comments: string[]
-    pluginData: object
+	globs: Glob[];
+	copyOnly: Glob[];
+	variable: object;
+	flags: object;
+	guid: string[];
+	syntax: Syntax[];
+	plugins: { [s: string]: string[] };
+	comments: string[];
+	pluginData: object;
 }
 
 interface ICyanParser {
-    Parse(p: Partial<CyanObject>): CyanSafe
+	Parse(p: Partial<CyanObject>): CyanSafe;
 }
-
 
 interface IFileSystemInstanceMetadata {
-    sourceAbsolutePath: string
-    destinationAbsolutePath: string
-    relativePath: string
+	sourceAbsolutePath: string;
+	destinationAbsolutePath: string;
+	relativePath: string;
 }
 
+const FileContent = Union({
+	String: of<string>(),
+	Buffer: of<Buffer>(),
+});
+
+type FileContent = typeof FileContent.T;
+
 interface FileSystemInstance {
-    metadata: IFileSystemInstanceMetadata
-    content: FileContent
-    parse: boolean
+	metadata: IFileSystemInstanceMetadata;
+	content: FileContent;
+	parse: boolean;
+}
+
+interface Glob {
+	root: string;
+	pattern: string[] | string;
+	ignore: string | string[];
 }
 
 interface IGlobFactory {
-    GenerateFiles(glob: Glob, target: string): IFileSystemInstanceMetadata[]
+	GenerateFiles(glob: Glob, target: string): IFileSystemInstanceMetadata[];
 
-    // Callback is used to bump progress
-    ReadFiles(files: IFileSystemInstanceMetadata[], callback?: Function): Promise<FileSystemInstance[]>
+	// Callback is used to bump progress
+	ReadFiles(files: IFileSystemInstanceMetadata[], callback?: Function): Promise<FileSystemInstance[]>;
 }
 
 interface IFileFactory {
-    CreateFileSystemInstance(relativePath: string, from?: string, to?: string): IFileSystemInstanceMetadata
+	CreateFileSystemInstance(relativePath: string, from?: string, to?: string): IFileSystemInstanceMetadata;
 
-    // Callback is used to bump progress
-    ReadFile(file: IFileSystemInstanceMetadata, callback?: Function): Promise<FileSystemInstance>
+	// Callback is used to bump progress
+	ReadFile(file: IFileSystemInstanceMetadata, callback?: Function): Promise<FileSystemInstance>;
 }
 
 interface IParsingStrategy {
+	ResolveContents(cyan: CyanSafe, files: FileSystemInstance[]): FileSystemInstance[];
 
-    ResolveContents(cyan: CyanSafe, files: FileSystemInstance[]): FileSystemInstance[]
+	Count(cyan: CyanSafe, files: FileSystemInstance[]): Map<string, number>;
 
-    Count(cyan: CyanSafe, files: FileSystemInstance[]): Map<string, number>
-
-	ResolveFiles(cyan: CyanSafe, files: IFileSystemInstanceMetadata[]): IFileSystemInstanceMetadata[]
+	ResolveFiles(cyan: CyanSafe, files: IFileSystemInstanceMetadata[]): IFileSystemInstanceMetadata[];
 }
+
+export {
+	Glob,
+	Syntax,
+	CyanObject,
+	CyanSafe,
+	ICyanParser,
+	IFileSystemInstanceMetadata,
+	FileSystemInstance,
+	IGlobFactory,
+	IFileFactory,
+	IParsingStrategy,
+};
