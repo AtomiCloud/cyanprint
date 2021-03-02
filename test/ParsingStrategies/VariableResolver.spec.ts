0ef1f369c6ad02d3ded57950e149c264b5159c96
Lucria <jerryzhang28eclipse@gmail.com>
2021-03-02 14:07:48 +0800

    [Test] Added tests for ResolveContents for VariableResolver

--- test/ParsingStrategies/VariableResolver.spec.ts
+++ test/ParsingStrategies/VariableResolver.spec.ts
@@ -143,6 +143,71 @@ describe("VariableResolver", () => {
         });
     });
 
+    describe("ResolveContents", () => {
+		it("should replace the content variable to the correct value", () => {
+			let path1: string = "root/var~a~/a.a";
+            let fileMeta1: IFileSystemInstanceMetadata = {
+                sourceAbsolutePath: path1,
+                destinationAbsolutePath: path1,
+                relativePath: path1,
+            }
+            let file1: FileSystemInstance = {
+                metadata: fileMeta1,
+                content: FileContent.String("line1\nvar~b.c~ are red\nvar~b.c~ are blue"),
+                parse: true,
+            };
+
+            let path2: string = "root/var~b.c~/var~g~";
+            let fileMeta2: IFileSystemInstanceMetadata = {
+                sourceAbsolutePath: path2,
+                destinationAbsolutePath: path2,
+                relativePath: path2,
+            }
+            let file2: FileSystemInstance = {
+                metadata: fileMeta2,
+                content: FileContent.String("line2\nvar~b.d.e~ help me!\nvar~b.c~ are blue\nvar~g~ are black!!"),
+                parse: true,
+            };
+
+            let path3: string = "root/var~b.d.e~/var~g~";
+            let fileMeta3: IFileSystemInstanceMetadata = {
+                sourceAbsolutePath: path3,
+                destinationAbsolutePath: path3,
+                relativePath: path3,
+            }
+            let file3: FileSystemInstance = {
+                metadata: fileMeta3,
+                content: FileContent.String("line2\nvar~b.d.f~ are red\nvar~b.c~ are blue\nvar~g~ are black!!"),
+                parse: true,
+            };
+
+			let testSubjects = [file1, file2, file3];
+
+            let file1Expected: FileSystemInstance = {
+                metadata: fileMeta1,
+                content: FileContent.String("line1\nViolets are red\nViolets are blue"),
+                parse: true,
+            };
+
+            let file2Expected: FileSystemInstance = {
+                metadata: fileMeta2,
+                content: FileContent.String("line2\nplease help me!\nViolets are blue\nOreos are black!!!"),
+                parse: true,
+            };
+
+            let file3Expected: FileSystemInstance = {
+                metadata: fileMeta3,
+                content: FileContent.String("line2\nApples are red\nViolets are blue\nOreos are black!!"),
+                parse: true,
+            };
+
+			let expected = [file1Expected, file2Expected, file3Expected];
+
+			let actual = variableResolver.ResolveContents(testCyanSafe, testSubjects);
+			actual.should.deep.equal(expected)
+		});
+	});
+
     describe("ModifyVariablesWithAllSyntax", () => {
         it("should pad variables with all open and close syntax terms", () => {
             variableResolver.ModifyVariablesWithAllSyntax("package.name", testCyanSafe.syntax).should.deep.equal(["var~package.name~"]);
