a24718c0bd62aa3e8ad8e8cc3322bf4f7c367015
Lucria <jerryzhang28eclipse@gmail.com>
2021-03-03 11:41:56 +0800

    [Test] Added test case for multi syntax for Count

--- test/ParsingStrategies/VariableResolver.spec.ts
+++ test/ParsingStrategies/VariableResolver.spec.ts
@@ -12,7 +12,7 @@ should();
 let core: Core = new Kore();
 core.ExtendPrimitives();
 
-let variables: object = {
+const variables: object = {
     a: "Roses",
     b: {
         c: "Violets",
@@ -24,7 +24,7 @@ let variables: object = {
     g: "Oreos"
 };
 
-let testCyanSafe: CyanSafe = {
+const testCyanSafe: CyanSafe = {
     comments: [],
     copyOnly: [],
     flags: {},
@@ -33,12 +33,24 @@ let testCyanSafe: CyanSafe = {
     pluginData: {},
     plugins: {},
     syntax: [["~", "~"]],
-    variable: variables
+    variable: variables,
 };
+
+const testCyanSafeMultiSyntax: CyanSafe = {
+    comments: [],
+    copyOnly: [],
+    flags: {},
+    globs: [],
+    guid: [],
+    pluginData: {},
+    plugins: {},
+    syntax: [["~", "~"], ["{", "}"], ["#", "#"]],
+    variable: variables,
+}
 
 describe("VariableResolver", () => {
     describe("Count", () => {
-        it("should count the number of occurrences of each variable", () => {
+        it("should count the number of occurrences of each variable correctly", () => {
             let path1: string = "root/var~a~/a.a";
             let fileMeta1: IFileSystemInstanceMetadata = {
                 sourceAbsolutePath: path1,
@@ -86,6 +98,60 @@ describe("VariableResolver", () => {
             ] as [string, number][]).Sort(SortType.AtoZ, (t: [string, number]) => t[0]);
 
             let actual: [string, number][] = variableResolver.Count(testCyanSafe, testSubject)
+                .SortByKey(SortType.AtoZ)
+                .Arr();
+
+            actual.should.deep.equal(expected);
+        });
+
+        it("should count the number of occurrences of each variable for all different syntaxes", () => {
+            let path1: string = "root/var~a~/a.a";
+            let fileMeta1: IFileSystemInstanceMetadata = {
+                sourceAbsolutePath: path1,
+                destinationAbsolutePath: path1,
+                relativePath: path1,
+            }
+            let file1: FileSystemInstance = {
+                metadata: fileMeta1,
+                content: FileContent.String("line1\nvar~b.c~ are red\nvar~b.c~ are blue"),
+                parse: true,
+            };
+
+            let path2: string = "root/var{b.c}/var#g#";
+            let fileMeta2: IFileSystemInstanceMetadata = {
+                sourceAbsolutePath: path2,
+                destinationAbsolutePath: path2,
+                relativePath: path2,
+            }
+            let file2: FileSystemInstance = {
+                metadata: fileMeta2,
+                content: FileContent.String("line2\nvar~b.d.e~ help me!\nvar#b.c# are blue\nvar{g} are black!!"),
+                parse: true,
+            };
+
+            let path3: string = "root/var{b.d.e}/var{g}";
+            let fileMeta3: IFileSystemInstanceMetadata = {
+                sourceAbsolutePath: path3,
+                destinationAbsolutePath: path3,
+                relativePath: path3,
+            }
+            let file3: FileSystemInstance = {
+                metadata: fileMeta3,
+                content: FileContent.String("line2\nvar#b.d.f# are red\nvar#b.c# are blue\nvar#g# are black!!"),
+                parse: true,
+            };
+
+            let testSubject = [file1, file2, file3];
+
+            let expected: [string, number][] = ([
+                ["a", 1],
+                ["b.c", 5],
+                ["b.d.e", 2],
+                ["b.d.f", 1],
+                ["g", 4]
+            ] as [string, number][]).Sort(SortType.AtoZ, (t: [string, number]) => t[0]);
+
+            let actual: [string, number][] = variableResolver.Count(testCyanSafeMultiSyntax, testSubject)
                 .SortByKey(SortType.AtoZ)
                 .Arr();
 
