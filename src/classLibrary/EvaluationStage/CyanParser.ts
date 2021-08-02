ca8c348f94cae4cf7ae124543dbd3d2974c2a61f
Jerry <jerry@eng.atomi.cloud>
2021-08-03 00:38:28 +0800

    refactor: reduces dependency on lodash by using Kore

--- src/classLibrary/EvaluationStage/CyanParser.ts
+++ src/classLibrary/EvaluationStage/CyanParser.ts
@@ -1,4 +1,4 @@
-import { CyanObject, CyanSafe, ICyanParser, Syntax } from "../interfaces/interfaces";
+import { CyanObject, CyanSafe, Glob, ICyanParser, Syntax } from "../interfaces/interfaces";
 import _ from "lodash";
 
 class CyanParser implements ICyanParser {
@@ -16,16 +16,16 @@ class CyanParser implements ICyanParser {
             pluginData: {}
         }
 
-        if (cyanObject.globs) defaultCyanSafe.globs = _.concat([], cyanObject.globs);
-        if (cyanObject.copyOnly) defaultCyanSafe.copyOnly = _.concat([], cyanObject.copyOnly);
+        if (cyanObject.globs) defaultCyanSafe.globs = ([] as Glob[]).Add(cyanObject.globs);
+        if (cyanObject.copyOnly) defaultCyanSafe.copyOnly = ([] as Glob[]).Add(cyanObject.copyOnly);
         if (cyanObject.variable) defaultCyanSafe.variable = cyanObject.variable;
         if (cyanObject.flags) defaultCyanSafe.flags = cyanObject.flags;
-        if (cyanObject.guid) defaultCyanSafe.guid = _.concat([], cyanObject.guid);
+        if (cyanObject.guid) defaultCyanSafe.guid = ([] as string[]).Add(cyanObject.guid);
         if (cyanObject.syntax) {
-            defaultCyanSafe.syntax = _.chunk(_.flattenDeep(cyanObject.syntax), 2) as Syntax[];
+            defaultCyanSafe.syntax = _.chunk(cyanObject.syntax.flat(), 2) as Syntax[];
         }
         if (cyanObject.plugins) defaultCyanSafe.plugins = cyanObject.plugins;
-        if (cyanObject.comments) defaultCyanSafe.comments = _.concat([], cyanObject.comments);
+        if (cyanObject.comments) defaultCyanSafe.comments = ([] as string[]).Add(cyanObject.comments);
         if (cyanObject.pluginData) defaultCyanSafe.pluginData = cyanObject.pluginData;
 
         return defaultCyanSafe;
